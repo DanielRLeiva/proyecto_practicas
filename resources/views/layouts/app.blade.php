a08f86f547a8ace7ed629f42e50a1a53126542ee
DanielRLeiva <daniley29@gmail.com>
2025-05-27 08:34:54 +0000

    Modificación del header, logo del centro y agregado menú desplegable de Usufructos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,31 @@
     {{-- Bootstrap CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    {{-- Font Awesome CSS --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+
     {{-- CSS adicional --}}
     @stack('styles')
+
+    <style>
+        .social-icon:hover {
+            color: #343a40 !important;
+            /* color más oscuro (dark gray) */
+        }
+    </style>
 </head>
 
 <body @auth data-role="{{ Auth::user()->getRoleNames()->first() }}" @endauth>
 
     {{-- NAVBAR --}}
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-info bg-info pt-4 pb-4">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('aulas.index') }}">Inventariado TIC CPIFP Alan Turing</a>
+    <header class="bg-info">
+        <nav class="navbar navbar-expand-lg pt-0 pb-0">
+            <div class="container d-flex align-items-center py-3">
+                <a class="navbar-brand bg-white px-3 py-2 rounded shadow-sm" href="{{ route('aulas.index') }}">
+                    <img decoding="async" class=" lazyloaded" width="150" src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_150/https://fpalanturing.es/wp-content/uploads/2024/01/CPIFPAT_logotipo_color.webp" data-src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_150/https://fpalanturing.es/wp-content/uploads/2024/01/CPIFPAT_logotipo_color.webp" alt="" title="Centro Público Integrado de Formación Profesional Alan Turing" style="vertical-align: middle;">
+                </a>
+
+                <h5>Inventariado TIC</h5>
 
                 {{-- Botón hamburguesa --}}
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContenido" aria-controls="navbarContenido" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +41,7 @@
                 </button>
 
                 {{-- Contenido colapsable --}}
-                <div class="collapse navbar-collapse justify-content-end" id="navbarContenido">
+                <div class="collapse navbar-collapse justify-content-end mt-3" id="navbarContenido">
                     <ul class="navbar-nav">
                         @auth
                         {{-- Enlaces visibles directamente en pantallas grandes --}}
@@ -35,7 +49,16 @@
                         <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Usuarios</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('auditoria.index') }}">Auditorías</a></li>
                         @endrole
-                        <li class="nav-item"><a class="nav-link" href="{{ route('usufructos.index') }}">Usufructos</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="usufructosDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Usufructos
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="usufructosDropdown">
+                                <li><a class="dropdown-item" href="{{ route('usufructos.index') }}">Usufructos</a></li>
+                                <li><a class="dropdown-item" href="{{ route('profesors.index') }}">Profesores</a></li>
+                                <li><a class="dropdown-item" href="{{ route('portatils.index') }}">Portátiles</a></li>
+                            </ul>
+                        </li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('equipos.all') }}">Todos los Equipos</a></li>
 
                         <li class="nav-item">
@@ -94,37 +117,27 @@
             <div class="mb-3 d-flex justify-content-center gap-4">
                 <!-- Facebook -->
                 <a href="https://www.facebook.com/cpifpalanturing" target="_blank" title="Facebook">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="grey" viewBox="0 0 24 24">
-                        <path d="M22.675 0h-21.35C.6 0 0 .6 0 1.337v21.326C0 23.4.6 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.894-4.788 4.659-4.788 1.325 0 2.464.097 2.797.141v3.24h-1.92c-1.504 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.622h-3.12V24h6.116C23.4 24 24 23.4 24 22.663V1.337C24 .6 23.4 0 22.675 0z" />
-                    </svg>
+                    <i class="fab fa-facebook-square fa-2x text-secondary social-icon"></i>
                 </a>
 
                 <!-- Twitter -->
                 <a href="https://twitter.com/cpifpalanturing" target="_blank" title="Twitter">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="grey" viewBox="0 0 24 24">
-                        <path d="M24 4.557a9.83 9.83 0 0 1-2.828.775 4.932 4.932 0 0 0 2.165-2.724c-.951.564-2.005.974-3.127 1.195A4.916 4.916 0 0 0 16.616 3c-2.717 0-4.924 2.206-4.924 4.924 0 .386.045.762.127 1.124-4.092-.205-7.719-2.165-10.148-5.144a4.822 4.822 0 0 0-.666 2.475c0 1.708.869 3.213 2.188 4.096a4.904 4.904 0 0 1-2.229-.616c-.054 1.985 1.397 3.841 3.444 4.256a4.934 4.934 0 0 1-2.224.084 4.928 4.928 0 0 0 4.604 3.417A9.867 9.867 0 0 1 0 19.54a13.94 13.94 0 0 0 7.548 2.212c9.058 0 14.01-7.513 14.01-14.01 0-.213-.005-.425-.014-.636A10.025 10.025 0 0 0 24 4.557z" />
-                    </svg>
+                    <i class="fab fa-twitter-square fa-2x text-secondary social-icon"></i>
                 </a>
 
                 <!-- LinkedIn -->
                 <a href="https://es.linkedin.com/company/cpifpalanturing" target="_blank" title="LinkedIn">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="grey" viewBox="0 0 24 24">
-                        <path d="M4.98 3.5C4.98 5.43 3.43 7 1.5 7S-1.98 5.43-1.98 3.5 0.57 0 2.5 0 4.98 1.57 4.98 3.5zM.5 8h4v16h-4V8zm7.5 0h3.64v2.02h.05c.51-.96 1.76-1.98 3.64-1.98 3.89 0 4.61 2.56 4.61 5.89V24h-4V14.7c0-2.22-.04-5.07-3.1-5.07-3.11 0-3.59 2.43-3.59 4.93V24h-4V8z" />
-                    </svg>
+                    <i class="fab fa-linkedin fa-2x text-secondary social-icon"></i>
                 </a>
 
                 <!-- Instagram -->
                 <a href="https://www.instagram.com/cpifpalanturing/" target="_blank" title="Instagram">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="grey" viewBox="0 0 24 24">
-                        <path d="M12 2.2c3.2 0 3.584.012 4.85.07 1.17.054 1.97.24 2.43.403a4.9 4.9 0 0 1 1.78 1.15 4.9 4.9 0 0 1 1.15 1.78c.163.46.35 1.26.403 2.43.058 1.266.07 1.65.07 4.85s-.012 3.584-.07 4.85c-.054 1.17-.24 1.97-.403 2.43a4.9 4.9 0 0 1-1.15 1.78 4.9 4.9 0 0 1-1.78 1.15c-.46.163-1.26.35-2.43.403-1.266.058-1.65.07-4.85.07s-3.584-.012-4.85-.07c-1.17-.054-1.97-.24-2.43-.403a4.9 4.9 0 0 1-1.78-1.15 4.9 4.9 0 0 1-1.15-1.78c-.163-.46-.35-1.26-.403-2.43C2.212 15.784 2.2 15.4 2.2 12s.012-3.584.07-4.85c.054-1.17.24-1.97.403-2.43a4.9 4.9 0 0 1 1.15-1.78 4.9 4.9 0 0 1 1.78-1.15c.46-.163 1.26-.35 2.43-.403C8.416 2.212 8.8 2.2 12 2.2zm0 1.8c-3.17 0-3.535.012-4.782.068-1.03.047-1.59.216-1.96.36a3.1 3.1 0 0 0-1.16.757 3.1 3.1 0 0 0-.757 1.16c-.144.37-.313.93-.36 1.96-.056 1.247-.068 1.612-.068 4.782s.012 3.535.068 4.782c.047 1.03.216 1.59.36 1.96.174.435.412.832.757 1.16.328.345.725.583 1.16.757.37.144.93.313 1.96.36 1.247.056 1.612.068 4.782.068s3.535-.012 4.782-.068c1.03-.047 1.59-.216 1.96-.36a3.1 3.1 0 0 0 1.16-.757 3.1 3.1 0 0 0 .757-1.16c.144-.37.313-.93.36-1.96.056-1.247.068-1.612.068-4.782s-.012-3.535-.068-4.782c-.047-1.03-.216-1.59-.36-1.96a3.1 3.1 0 0 0-.757-1.16 3.1 3.1 0 0 0-1.16-.757c-.37-.144-.93-.313-1.96-.36-1.247-.056-1.612-.068-4.782-.068zm0 3.3a6.5 6.5 0 1 1 0 13 6.5 6.5 0 0 1 0-13zm0 2.2a4.3 4.3 0 1 0 0 8.6 4.3 4.3 0 0 0 0-8.6zm6.6-2.4a1.2 1.2 0 1 1 0 2.4 1.2 1.2 0 0 1 0-2.4z" />
-                    </svg>
+                    <i class="fab fa-instagram-square fa-2x text-secondary social-icon"></i>
                 </a>
 
                 <!-- YouTube -->
                 <a href="https://www.youtube.com/@cpifpalanturing" target="_blank" title="YouTube">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="grey" viewBox="0 0 24 24">
-                        <path d="M23.5 6.2s-.2-1.6-.8-2.3c-.8-.9-1.7-.9-2.1-1C17.6 2.5 12 2.5 12 2.5h-.1s-5.6 0-8.6.4c-.4.1-1.3.1-2.1 1-.6.7-.8 2.3-.8 2.3S0 8.2 0 10.2v1.7c0 2 .2 4 .2 4s.2 1.6.8 2.3c.8.9 1.9.9 2.4 1 1.8.2 7.6.4 7.6.4s5.6 0 8.6-.4c.4-.1 1.3-.1 2.1-1 .6-.7.8-2.3.8-2.3s.2-2 .2-4v-1.7c0-2-.2-4-.2-4zM9.5 15.5v-7l6.5 3.5-6.5 3.5z" />
-                    </svg>
+                    <i class="fab fa-youtube-square fa-2x text-secondary social-icon"></i>
                 </a>
             </div>
 
